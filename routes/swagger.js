c77c8e7dc8b116a36f2eecafe6aba6df583fc7aa
Levi Omagbemi <leviomagbemi15@gmail.com>
2026-04-04 16:40:28 +0100

    fixed cors error and update swagger base url

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-output.json');
 
+// Dynamically override host and schemes based on the environment
+swaggerDocument.host = process.env.APP_BASE_URL || 'localhost:8080';
+swaggerDocument.schemes = process.env.NODE_ENV === 'production' ? ['https'] : ['http', 'https'];
+
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
